ceb11eb1a15229a9af93dd4641779f1e452c2aa1
Adem <ado.designs@outlook.de>
2025-04-18 21:37:38 +0200

    modify functions with clean coding

--- script.js
+++ script.js
@@ -76,59 +76,72 @@ function init() {
 
 // zeigt die aktuelle frage und antwort in html an
 function showQuestion() {
-
     // zähler ab 7 frage abbrechen ( end-screen)
-    if (currentQuestion >= questions.length) {
-        // show end screen
-        // take step 1,2,3 from html & show quiz beendet after click last question
-        document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style = 'display: none';
-
-        // wie viele fragen wurden beantwortet
-        document.getElementById('amountOfQuestions').innerHTML = questions.length;
-
-        // wie viele fragen wurden richtig beantwortet step 1
-        // step 2 variable erstellen 
-        document.getElementById('currentRightQuestion').innerHTML = rightQuestions;
-
-        // quiz img ersetzt mit quiz background .src 
-        document.getElementById('head-content-img').src = './img/quiz-background.png';
-
-    } else { //show question
-
-        // prozentausrechnen
-
-        let percent = (currentQuestion + 1) / questions.length;
-        // ausgerechnete zahl aufrunden mit  math.round
-        percent = Math.round(percent * 100);
-        console.log('fortschrit:', percent);
-
-        // die zahl die ausgrechnet wurde in bar anzeigen lassen 14.234234%
-        document.getElementById('progress-bar-id').innerHTML = `${percent} %`;
-        // progresbar balken schicker ausehenlassen mit % 
-        document.getElementById('progress-bar-id').style = `width: ${percent}%`;
-
-        //greift auf die aktuelle frage aus dem array 
-        let question = questions[currentQuestion];
-
-        //greifen auf die id questiontext + schreiben text der frage in dieses html aus dem frage object
-        document.getElementById('questionText').innerHTML = question['question']
-
-        // id anwser aus html ziehen + in object attribute zuweisen zur jeder id in html
-        // suchen 4 html elemente mit ids + schreiben die antwort aus dem frage objekt
-        document.getElementById('answer_1').innerHTML = question['answer_1']
-        document.getElementById('answer_2').innerHTML = question['answer_2']
-        document.getElementById('answer_3').innerHTML = question['answer_3']
-        document.getElementById('answer_4').innerHTML = question['answer_4']
-
-        // step nummer der fragen anzeigen
-
-        // step 1
-        document.getElementById('current-question').innerHTML = currentQuestion + 1;
+    if (gameIsOver()) {
+        showEndScreen();
+    } else {
+        //show question
+        updateProgressBar();
+        updateToNextQuestion();
     }
-
-
 };
+
+function gameIsOver() {
+    //wenn currenquestion gleich größer als 7 ist dann gameisover true
+    return currentQuestion >= questions.length
+}
+function showEndScreen() {
+    // show end screen
+    // take step 1,2,3 from html & show quiz beendet after click last question
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+
+    // wie viele fragen wurden beantwortet
+    document.getElementById('amountOfQuestions').innerHTML = questions.length;
+
+    // wie viele fragen wurden richtig beantwortet step 1
+    // step 2 variable erstellen 
+    document.getElementById('currentRightQuestion').innerHTML = rightQuestions;
+
+    // quiz img ersetzt mit quiz background .src 
+    document.getElementById('head-content-img').src = './img/quiz-background.png';
+}
+function updateProgressBar() {
+    // prozentausrechnen
+    // updates prozentbalken
+
+    let percent = (currentQuestion + 1) / questions.length;
+    // ausgerechnete zahl aufrunden mit  math.round
+    percent = Math.round(percent * 100);
+    // console.log('fortschrit:', percent);
+
+    // die zahl die ausgrechnet wurde in bar anzeigen lassen 14.234234%
+    document.getElementById('progress-bar-id').innerHTML = `${percent} %`;
+    // progresbar balken schicker ausehenlassen mit % 
+    document.getElementById('progress-bar-id').style = `width: ${percent}%`;
+}
+
+function updateToNextQuestion() {
+    // zeigt die nächste frag an
+    //greift auf die aktuelle frage aus dem array 
+    let question = questions[currentQuestion];
+
+    //greifen auf die id questiontext + schreiben text der frage in dieses html aus dem frage object
+    document.getElementById('questionText').innerHTML = question['question']
+
+    // id anwser aus html ziehen + in object attribute zuweisen zur jeder id in html
+    // suchen 4 html elemente mit ids + schreiben die antwort aus dem frage objekt
+    document.getElementById('answer_1').innerHTML = question['answer_1']
+    document.getElementById('answer_2').innerHTML = question['answer_2']
+    document.getElementById('answer_3').innerHTML = question['answer_3']
+    document.getElementById('answer_4').innerHTML = question['answer_4']
+
+    // step nummer der fragen anzeigen
+
+    // step 1
+    document.getElementById('current-question').innerHTML = currentQuestion + 1;
+}
+
 // herausfinden beim clicken auf button richtig oder falsch 
 // function wird aufgerufen wenn eine antwort ausgewählt wurde der parameter ist die id der angeklickten antwort
 function answer(selection) {
@@ -152,7 +165,7 @@ function answer(selection) {
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
     //vergleichen ob die antwort übereinstimmmt
-    if (selectedQuestionNumber == question['right_answer']) {
+    if (rightAnswerSelected(selectedQuestionNumber)) {
         // loggen aus wenn die antwort übereinstimmt
         // console.log('Richtige Antwort!');
 
@@ -181,6 +194,10 @@ function answer(selection) {
     // button disable aktivieren
     document.getElementById('next-btn').disabled = false;
 };
+
+function rightAnswerSelected(selectedQuestionNumber) {
+    return selectedQuestionNumber == question['right_answer']
+}
 
 function nextQuestion() {
 
